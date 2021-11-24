fa3d48285538665d2c9a30a47874ab8b2e874f02
Carlos Mendes <cmendesce@gmail.com>
2021-11-24 20:08:29 -0300

    Add printing status

--- orchestrator/app.py
+++ orchestrator/app.py
@@ -4,7 +4,7 @@ import requests
 
 from utils import expand_config_template
 
-def main(conf_file_path):
+def build_scenarios(conf_file_path):
     conf_file = open(conf_file_path, 'r')
     conf = json.load(conf_file)
 
@@ -13,28 +13,45 @@ def main(conf_file_path):
     users = conf['concurrentUsers']
     rounds = conf['rounds']
     envoy_host = conf['envoy_host']
+    scenarios = []
 
     for failure in failure_rate:
-        update_env(envoy_host, failure)
-        results = []
         for pattern_template in patterns:
             config_templates = expand_config_template(pattern_template['config_template'])
             for config_template in config_templates:
-                do_test(config_template, pattern_template, 1) # warm up application
                 for user in users:
-                    for round in range(1, rounds + 1):
-                        result = do_test(config_template, pattern_template, user)
-                        if result is None:
-                            break
-                        for r in result:
-                            r['users'] = user
-                            r['round'] = round
-                            r['lib'] = pattern_template['lib']
-                            r['failure_rate'] = failure
-                            for config_key in config_template.keys():
-                                r[config_key] = config_template[config_key]
-                        results += result
-  
+                    for round in range(1, rounds):
+                        scenarios.append({
+                            'config_template': config_template,
+                            'pattern_template': pattern_template,
+                            'user': user,
+                            'round': round,
+                            'failure': failure,
+                            'envoy_host': envoy_host
+                        })
+    return scenarios
+
+
+def main(conf_file_path):
+    scenarios = build_scenarios(conf_file_path)
+    results = []
+    for idx, scenario in enumerate(scenarios):
+        print(f'staring {idx} test')
+        user = scenario['user']
+        pattern_template = scenario['pattern_template']
+        failure = scenario['failure']
+        config_template = scenario['config_template']
+        result = do_test(config_template, pattern_template, user)
+        if result is None:
+            break
+        for r in result:
+            r['users'] = user
+            r['round'] = round
+            r['lib'] = pattern_template['lib']
+            r['failure_rate'] = failure
+            for config_key in config_template.keys():
+                r[config_key] = config_template[config_key]
+        results += result
     df = pd.DataFrame(results)
     df.to_csv('./test.csv', index=False)
 
