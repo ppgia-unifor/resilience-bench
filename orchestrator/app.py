4b7a779e2013b1df41037eef037a2742d13d35a4
Carlos Mendes <cmendesce@gmail.com>
2021-12-07 09:19:44 -0300

    Add thread control into orchestrator

--- orchestrator/app.py
+++ orchestrator/app.py
@@ -1,104 +1,111 @@
-import pandas as pd
+import os
 import json
 import requests
+import concurrent
 from urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
 import logging
-import os
+from datetime import datetime
 from utils import expand_config_template
 from storage import save_file
 from notifier import notify
-from uuid import uuid4
 
 logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 requestsSesstion = requests.Session()
-
 retries = Retry(total=10,
                 backoff_factor=0.1,
                 status_forcelist=[ 500, 502, 503, 504 ])
 requestsSesstion.mount('http://', HTTPAdapter(max_retries=retries))
 
+test_id = datetime.now().strftime('%c')
+
 def build_scenarios():
     conf_file = open(os.environ.get('CONFIG_FILE'), 'r')
     conf = json.load(conf_file)
 
-    failure_rate = conf['failure_rate']
+    failure_rate = conf['failureRate']
     patterns = conf['patterns']
     users = conf['concurrentUsers']
     rounds = conf['rounds'] + 1
-    envoy_host = conf['envoy_host']
+    envoy_host = conf['envoyHost']
     scenarios = []
 
     for failure in failure_rate:
         for pattern_template in patterns:
-            config_templates = expand_config_template(pattern_template['config_template'])
+            config_templates = expand_config_template(pattern_template['configTemplate'])
             for config_template in config_templates:
                 for user in users:
                     for idx_round in range(1, rounds):
                         scenarios.append({
-                            'config_template': config_template,
-                            'pattern_template': pattern_template,
-                            'user': user,
+                            'configTemplate': config_template,
+                            'patternTemplate': pattern_template,
+                            'users': user,
                             'round': idx_round,
                             'failure': failure,
-                            'envoy_host': envoy_host
+                            'envoyHost': envoy_host
                         })
+    logger.info(f'{len(scenarios)} scenarios generated')
+    save_file(f'{test_id}/scenarios', scenarios, 'json')
     return scenarios
 
-
 def main():
-    test_id = uuid4()
     scenarios = build_scenarios()
     results = []
-    total_scenarios = len(scenarios)
-    for idx, scenario in enumerate(scenarios):
-        user = scenario['user']
-        pattern_template = scenario['pattern_template']
-        failure = scenario['failure']
-        config_template = scenario['config_template']
-        round = scenario['round']
-        
-        logger.info(f'Round [{idx}/{total_scenarios}] Users {user} Pattern {pattern_template["name"]}')
-
-        status_code, result = do_test(config_template, pattern_template, user)
-        if status_code != 200:
-            result = [{
-                'error': True,
-                'error_message': result
-            }]
-        
-        for item_result in result:
-            item_result['users'] = user
-            item_result['round'] = round
-            item_result['lib'] = pattern_template['lib']
-            item_result['name'] = pattern_template['name']
-            item_result['failure_rate'] = failure
-            for config_key in config_template.keys():
-                item_result[config_key] = config_template[config_key]
-        
-        results += result
-
-        if idx % 100 == 0:
-            save_file(f'{test_id}/{idx}', results)
-    save_file(f'{test_id}/total', results)
+    for scenario in scenarios:
+        users = scenario['users'] + 1
+        with concurrent.futures.ThreadPoolExecutor(max_workers=users) as executor:
+            futures = []
+            for user_id in range(1, users):
+                futures.append(executor.submit(do_test, scenario=scenario, user_id=user_id))
+            
+            for future in concurrent.futures.as_completed(futures):
+                results.append(future.result())
+            
+    save_file(f'{test_id}/total', results, 'csv')
     notify(f'{test_id} - teste finalizado')
 
 def update_env(server_host, failure):
     pass
 
-def do_test(config_template, pattern, users):
-    payload = {
-            'concurrentUsers': users,
-            'maxRequestsAllowed': 1000,
-            'targetSuccessfulRequests': 25,
-            'params': config_template
-        }
-    response = requestsSesstion.post(pattern['url'], data=json.dumps(payload), headers={'Content-Type': 'application/json'})
-    result = response.text
+def do_test(scenario, user_id):
+    start_time = datetime.now()
+    users = scenario['users']
+    pattern_template = scenario['patternTemplate']
+    failure = scenario['failure']
+    config_template = scenario['configTemplate']
+    round = scenario['round']
+
+    payload = json.dumps({
+        'concurrentUsers': users,
+        'maxRequestsAllowed': 1000,
+        'targetSuccessfulRequests': 25,
+        'params': config_template
+    })
+    response = requestsSesstion.post(
+        pattern_template['url'], 
+        data=payload, 
+        headers={'Content-Type': 'application/json'}
+    )
+    result = {}
     if response.status_code == 200:
         result = response.json()
-
-    return response.status_code, result
-
+        result['userId'] = user_id
+        result['startTime'] = start_time
+        result['endTime'] = datetime.now()
+        result['users'] = users
+        result['round'] = round
+        result['lib'] = pattern_template['lib']
+        result['name'] = pattern_template['name']
+        result['failureRate'] = failure
+        for config_key in config_template.keys():
+            result[config_key] = config_template[config_key]
+    else:
+        result['error'] = True
+        result['errorMessage'] = response.text
+        result['statusCode'] = response.status_code
+    
+    return result
+    
 main()
--- orchestrator/storage.py
+++ orchestrator/storage.py
@@ -10,12 +10,19 @@ s3 = boto3.resource('s3')
 BUCKET_NAME = environ.get('AWS_BUCKET_NAME')
 OUTPUT_PATH = environ.get('OUTPUT_PATH')
 
-def save_file(filename, data):
+def save_file(filename, data, format):
     df = pd.DataFrame(data)
-    csv_buffer = StringIO()
-    df.to_csv(csv_buffer, index=False)
+    buffer = StringIO()
+
+    if format == 'csv':
+        df.to_csv(buffer, index=False)
+    elif format == 'json':
+        df.to_json(buffer)
+    else:
+        raise ValueError(f'format {format} not supported')
+
     try:
-        s3.Object(BUCKET_NAME, f'{OUTPUT_PATH}/{filename}.csv').put(Body=csv_buffer.getvalue())
+        s3.Object(BUCKET_NAME, f'{OUTPUT_PATH}/{filename}.{format}').put(Body=buffer.getvalue())
         logger.info('File saved in s3')
     except ClientError as e:
         logger.error('File could not be saved in s3')
