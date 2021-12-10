3eb6db3780d4715afc1501a2a3bccf66df9ee3dd
Carlos Mendes <cmendesce@gmail.com>
2021-12-09 21:52:57 -0300

    fix: add missing import

--- clients/resilience4j/src/main/java/br/unifor/ppgia/resilience4j/GeneralConfig.java
+++ clients/resilience4j/src/main/java/br/unifor/ppgia/resilience4j/GeneralConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import java.time.Duration;
 
 @Configuration
 public class GeneralConfig {
@@ -12,6 +13,5 @@ public class GeneralConfig {
         return restTemplateBuilder
            .setConnectTimeout(Duration.ofSeconds(2))
            .build();
-        //return builder.build();
     }
 }
