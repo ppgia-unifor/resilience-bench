980f97ae2d318373ec192e7c2cf459cc90d0c183
Carlos Mendes <cmendesce@gmail.com>
2021-12-13 11:14:34 -0300

    Add timeout to polly

--- clients/polly/Startup.cs
+++ clients/polly/Startup.cs
@@ -25,11 +25,18 @@ namespace ResiliencePatterns.Polly
                 opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
             });
 
-            var backendHost = Environment.GetEnvironmentVariable("BACKEND_HOST") ?? Configuration.GetValue<string>("BACKEND_HOST");
-            
+            var readTimeout = Environment.GetEnvironmentVariable("READ_TIMEOUT") ?? "0";
+            var backendHost = Environment.GetEnvironmentVariable("BACKEND_HOST") ?? "http://localhost:9211";
+            var resourcePath = Environment.GetEnvironmentVariable("RESOURCE_PATH") ?? "/status/200";
+
+            // logger.LogInformation("Read timeout is {0}.", readTimeout);
+            // logger.LogInformation("Backend host is {0}.", backendHost);
+            // logger.LogInformation("Resource is {0}.", resourcePath);
+
             services.AddHttpClient("backend", c =>
             {
                 c.BaseAddress = new Uri(backendHost);
+                c.Timeout = TimeSpan.FromSeconds(Int32.Parse(readTimeout));
             });
             services.AddScoped<BackendService>();
         }
