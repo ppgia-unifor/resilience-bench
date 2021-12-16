83b89f4f5e6fe78ca653412692de050eae76720c
Carlos Mendes <cmendesce@gmail.com>
2021-12-16 08:46:16 -0300

    fix: adapt to new test setup

--- orchestrator/app.py
+++ orchestrator/app.py
@@ -19,7 +19,7 @@ requestsSesstion = requests.Session()
 retries = Retry(total=10,
                 backoff_factor=0.1,
                 status_forcelist=[ 500, 502, 503, 504 ])
-requestsSesstion.mount('http://', HTTPAdapter(max_retries=retries))
+requestsSesstion.mount('http://', HTTPAdapter(max_retries=retries, pool_maxsize=500))
 
 test_id = datetime.now().astimezone(timezone('America/Sao_Paulo')).strftime('%c')
 
--- orchestrator/envoy.py
+++ orchestrator/envoy.py
@@ -12,24 +12,16 @@ def get_envoy():
             return container
     return None    
 
-def update_percentage_fault(percentage):
-    envoy = get_envoy()
-    envoy.exec_run(f'bash update_percentage_fault.sh {percentage}')
-    logging.info(f'update envoy to delay {percentage}% of requests')
-    time.sleep(1)
-
 def update_duration_fault(duration):
     envoy = get_envoy()
     envoy.exec_run(f'bash update_duration_fault.sh {duration}')
     logging.info(f'update envoy to delay {duration}ms')
-    time.sleep(1)
+    time.sleep(0.5)
 
 
-def update_delay_fault_injection(percentage):
-    client = docker.DockerClient(base_url='unix://var/run/docker.sock')
-    for container in client.containers.list():
-        if container.name == 'envoy':
-            container.exec_run(f'bash update_delay_fault_injection.sh {percentage}')
-            logging.info(f'update envoy to delay {percentage}% of requests')
-            time.sleep(1)
+def update_percentage_fault(percentage):
+    envoy = get_envoy()
+    envoy.exec_run(f'bash update_percentage_fault.sh {percentage}')
+    logging.info(f'update envoy to delay {percentage}%')
+    time.sleep(0.5)
             
