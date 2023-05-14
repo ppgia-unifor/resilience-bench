c5586c6a92e7057629619941c114f01a17e0c5b5
Carlos Mendes <cmendesce@gmail.com>
2023-05-14 15:16:51 -0300

    Add dockerfile

--- clients/cockatiel/src/Config.ts
+++ clients/cockatiel/src/Config.ts
@@ -2,6 +2,6 @@
 export class Config {
   public maxRequests: number = 0;
   public successfulRequests: number = 0;
-  public targetUrl: string;
+  public targetUrl: string = '';
   public patternParams: any;
 }
--- clients/cockatiel/src/index.ts
+++ clients/cockatiel/src/index.ts
@@ -1,17 +1,15 @@
 import express, { Express } from 'express';
-import dotenv from 'dotenv'
 import routerBaseline from './baseline/BaselineController';
 import routerRetry from './pattern/retry/RetryController';
 import routerCircuitBreaker from './pattern/circuitBreaker/CircuitBreakerController';
-dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.use('/', routerBaseline);
 app.use('/', routerRetry);
 app.use('/', routerCircuitBreaker);
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+  console.log(`Server is running at https://localhost:${port}`);
 });
