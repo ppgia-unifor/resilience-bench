ac1daae5f28381315c58c10005bf7ff0228179c3
Carlos Mendes <cmendesce@gmail.com>
2023-05-30 21:01:59 -0300

    Update index.ts

--- clients/cockatiel/src/index.ts
+++ clients/cockatiel/src/index.ts
@@ -4,7 +4,7 @@ import routerRetry from './pattern/retry/RetryController';
 import routerCircuitBreaker from './pattern/circuitBreaker/CircuitBreakerController';
 
 const app: Express = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 
 app.use('/', routerBaseline);
 app.use('/', routerRetry);
